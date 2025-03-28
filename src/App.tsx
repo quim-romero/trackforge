88c710952e576e9c1213d5c1b3eb46a6af817ffa
quim-romero <quimromero22@gmail.com>
2025-03-28 13:10:00 +0100

    feat: wire public routes with Home, Login and About

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import OnboardingLayout from "./components/OnboardingLayout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
 
 export default function App() {
   return (
     <Router>
-      <Routes></Routes>
+      <Routes>
+        <Route element={<OnboardingLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
