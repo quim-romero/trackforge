34daf3ddb0735b4526ccfdb5671f422bca141689
quim-romero <quimromero22@gmail.com>
2025-03-31 10:40:00 +0200

    feat: wire protected routes into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import OnboardingLayout from "./components/OnboardingLayout";
+import Dashboard from "./pages/Dashboard";
+import Tasks from "./pages/Tasks";
+import Stats from "./pages/Stats";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import About from "./pages/About";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 export default function App() {
   return (
@@ -13,6 +19,20 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
+        </Route>
+
+        <Route
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/stats" element={<Stats />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
         </Route>
       </Routes>
     </Router>
