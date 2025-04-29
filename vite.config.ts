344960a91c11f6e06b98308ddfdf9a779f77795a
quim-romero <quimromero22@gmail.com>
2025-04-29 15:00:00 +0200

    perf: Add bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    process.env.ANALYZE
+      ? visualizer({
+          filename: "dist/stats.html",
+          template: "treemap",
+          gzipSize: true,
+          brotliSize: true,
+          open: false,
+        })
+      : undefined,
+  ].filter(Boolean),
   server: {
     port: 5173,
     strictPort: true,
